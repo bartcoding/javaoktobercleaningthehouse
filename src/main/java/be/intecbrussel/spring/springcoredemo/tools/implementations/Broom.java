56669b79093ecedb7df11989c424111a56005779
BartHu <hunerbeinbart@gmail.com>
2020-06-02 11:52:05 +0200

    add garden hose use qualifiers and primary annotations

--- src/main/java/be/intecbrussel/spring/springcoredemo/tools/implementations/Broom.java
+++ src/main/java/be/intecbrussel/spring/springcoredemo/tools/implementations/Broom.java
@@ -1,11 +1,32 @@
 package be.intecbrussel.spring.springcoredemo.tools.implementations;
 
 import be.intecbrussel.spring.springcoredemo.tools.CleaningTool;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 public class Broom implements CleaningTool {
 
 
+    private String brandName;
+    private int broomLength;
+    public int getBroomLength() {
+        return broomLength;
+    }
+    @Value("#{160}")
+    public void setBroomLength(int broomLength) {
+        this.broomLength = broomLength;
+    }
 
+
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    @Value("#{'sweeper'}")
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
 
     public Broom() {
         System.out.println("constructing a broom bean");
